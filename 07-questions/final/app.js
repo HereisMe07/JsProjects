cf75a3145c6c5cf00bedadd3e0dcf8e3a22d4aa1
IT_TEK_VITU\Nssf_User <tujames77@gmail.com>
2025-11-20 16:06:54 +0700

    update

--- 07-questions/final/app.js
+++ 07-questions/final/app.js
@@ -1,6 +1,23 @@
 //using selectors inside the element
-const questions = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question"); //👉 This gives you a NodeList of all FAQ question blocks.
 
-questions.forEach(function (question) {
-   const btn = question
+// 👉 You are looping one by one through each .question.
+questions.forEach(function (question) { // question = the current question element.
+   // inside each .question, there is a .question-btn
+   const btn = question.querySelector(".question-btn");
+   // console.log(btn);
+
+   btn.addEventListener("click", function () { // When user clicks this button → run the function
+      // console.log(question);
+   
+      questions.forEach(function (item) {
+         if (item !== question) {
+            item.classList.remove("show-text");
+         }
+      });
+      // First click = turn ON && Second click = turn OFF
+      question.classList.toggle("show-text"); // The code adds a class called "show-text" 👉 That class makes the answer appear
+      // If .show-text NOT there → ADD it→ Answer becomes visible.
+      // If .show-text there → REMOVE it → Answer becomes invisible.
+   });
 });
